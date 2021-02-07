7e397429bd5ea11f7f00bbf1d7d67e0cb2488d00
Aanand Kainth <aanand@akainth.me>
2021-02-07 06:53:06 -0800

    Integrate sktime ARIMA model
    Co-authored-by: Chau Vu <37096589+cqvu@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-from sklearn import datasets, svm
-
 from sktime_forecast_bento import SktimeForecastBento
 
-iris = datasets.load_iris()
-X, y = iris.data, iris.target
-
-clf = svm.SVC(gamma="scale")
-clf.fit(X, y)
-
 service = SktimeForecastBento()
-service.pack('model', clf)
-service.save()
+
+service.start_dev_server()
+
+while True:
+    pass
--- sktime_forecast_bento.py
+++ sktime_forecast_bento.py
@@ -1,11 +1,30 @@
+import yfinance as yf
 from bentoml import env, artifacts, api, BentoService
 from bentoml.adapters import JsonInput
-from bentoml.frameworks.sklearn import SklearnModelArtifact
+from pandas._libs.tslibs.period import Period
+from sktime.forecasting.arima import ARIMA
+from sktime.forecasting.base import ForecastingHorizon
+import pandas as pd
 
 
 @env(infer_pip_packages=True)
-@artifacts([SklearnModelArtifact('model')])
+@artifacts([])
 class SktimeForecastBento(BentoService):
     @api(input=JsonInput())
     def predict(self, input):
-        return self.artifacts.model.predict(input)
+        stock = yf.Ticker(input["ticker"])
+        hist = stock.history(period="max")
+        hist.index = hist.index.map(lambda date: date.to_period(freq="M"))
+        data = hist[~hist.index.duplicated(keep="first")]["Close"]
+
+        forecaster = ARIMA(
+            order=(3, 1, 0), seasonal_order=(0, 1, 0, 12),
+            suppress_warnings=True
+        )
+        forecaster.fit(data)
+
+        fh = ForecastingHorizon(pd.to_datetime(["2021-03", "2021-04", "2021-05"]).map(lambda date: date.to_period(freq="M")), is_relative=False)
+        y_pred = forecaster.predict(fh)
+
+        y_pred.index = y_pred.index.map(lambda date: str(date))
+        return y_pred.to_dict()
